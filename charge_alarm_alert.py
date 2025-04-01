be75ccfe7035f0c60c1873b270bd6622826ca5a2
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-02 00:19:16 +0530

    Example code completion

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -13,5 +13,11 @@ import tkinter as tk          # tkinter imported
 # import tkinter
 # from tkinter.constants import *
 # tk = tkinter.Tk()
-
+# frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
+# frame.pack(fill=BOTH,expand=1)
+# label = tkinter.Label(frame, text="Hello, World")
+# label.pack(fill=X, expand=1)
+# button = tkinter.Button(frame,text="Exit",command=tk.destroy)
+# button.pack(side=BOTTOM)
+# tk.mainloop()
 
