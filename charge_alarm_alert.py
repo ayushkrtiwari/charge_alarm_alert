19f7f4c0f3089f4075267388d4769679fc31cc2c
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-02 00:20:56 +0530

    tkinter explanation over

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -1,23 +1,4 @@
 import os
 import time  
 from PIL import Image, ImageTk
-import tkinter as tk          # tkinter imported
-# Widgets are positioned with one of the geometry managers Place, Pack
-# or Grid. These managers can be called with methods place, pack, grid
-# available in every Widget.
-
-# Actions are bound to events by resources (e.g. keyword argument
-# command) or with the method bind.
-
-# Example (Hello, World):
-# import tkinter
-# from tkinter.constants import *
-# tk = tkinter.Tk()
-# frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
-# frame.pack(fill=BOTH,expand=1)
-# label = tkinter.Label(frame, text="Hello, World")
-# label.pack(fill=X, expand=1)
-# button = tkinter.Button(frame,text="Exit",command=tk.destroy)
-# button.pack(side=BOTTOM)
-# tk.mainloop()
-
+import tkinter as tk 
