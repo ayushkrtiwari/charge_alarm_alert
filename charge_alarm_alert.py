fb5909d41ef57bc00af57a016a046884967f8ebc
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-03 23:49:02 +0530

    import tkinter module messagebox

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -2,4 +2,4 @@ import os
 import time  # Import the time module
 from PIL import Image, ImageTk
 import tkinter as tk # Control display, positioning, control of widgets. TK is top level widget.
-
+from tkinter import messagebox
