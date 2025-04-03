508ba46fed1a32e023ef494cfce824ec65738d2a
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-03 23:55:35 +0530

    add psutil library

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -3,4 +3,5 @@ import time  # Import the time module
 from PIL import Image, ImageTk
 import tkinter as tk # Control display, positioning, control of widgets. TK is top level widget.
 from tkinter import messagebox
-from tkinter.ttk import Progressbar  # will show battery charge percentage
+from tkinter.ttk import Progressbar
+import psutil
