3ac844ebb35a1e96d76be938af455a68eb66465d
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-06 02:29:28 +0530

    Self property stored tagged

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -21,4 +21,4 @@ class ChargeAlarmApp:
         self.battery_image_label = tk.Label(root)
         self.battery_image_label.pack(pady=10)
 
-        self.battery_label = tk.Label(root, text="", font=("Helvetica", 14))
+        self.battery_label = tk.Label(root, text="", font=("Helvetica", 14)) # Stored as self property
