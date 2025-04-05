893e23b1574c0a0a6f488417972c67459afc52f0
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-06 01:24:24 +0530

    Comment for clarity

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -19,4 +19,4 @@ class ChargeAlarmApp:
         self.label.pack(pady=10)
 
         self.battery_image_label = tk.Label(root)
-        self.battery_image_label.pack(pady=10)
+        self.battery_image_label.pack(pady=10) # Battery Image Widget Pack as padding at y be 10 px
