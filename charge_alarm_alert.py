38dad08a5f9833cc8671d96ebadeec28c5a44fac
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-07 22:52:35 +0530

    Progress bar padding tag

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -25,6 +25,6 @@ class ChargeAlarmApp:
         self.battery_label.pack(pady=10)
         
         self.progress = Progressbar(root, orient=tk.HORIZONTAL, length=300, mode='determinate')
-        self.progress.pack(pady=10)
+        self.progress.pack(pady=10) # Battery Progress bar padding by 10 
         
         
