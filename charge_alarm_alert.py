a35620130f67c5dd50824137922b90a5466a8924
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-10 22:58:05 +0530

    Stop button state defined

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -30,5 +30,5 @@ class ChargeAlarmApp:
         self.start_button = tk.Button(root, text="Start Monitoring", command=self.start_monitoring, font=("Helvetica", 12))
         self.start_button.pack(pady=5)
 
-        tk.Button(root, text="Stop Monitoring", command=self.stop_monitoring)
+        tk.Button(root, text="Stop Monitoring", command=self.stop_monitoring, state=tk.DISABLED)
         
