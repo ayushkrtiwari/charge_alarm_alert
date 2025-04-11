a7405c1b3f8acf061c4e948deea84bb3d855f7fb
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-11 23:59:27 +0530

    Stop Alarm Padding 5

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -34,6 +34,6 @@ class ChargeAlarmApp:
         self.stop_button.pack(pady=5)
         
         self.stop_alarm_button = tk.Button(root, text="Ctrl+Enter", state=tk.DISABLED, font=("Helvetica", 12))
-        self.stop_alarm_button.pack())
+        self.stop_alarm_button.pack(pady=5)
         
         
