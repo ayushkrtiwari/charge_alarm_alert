808c99011d9d18be734a648d428aabbb9293a287
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-17 23:26:56 +0530

    Search for image: empty_battery.png

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -50,7 +50,7 @@ class ChargeAlarmApp:
         # Load images
         try:
             self.battery_images = {
-                'empty': Image.open(os.path.join(base_path)),
+                'empty': Image.open(os.path.join(base_path, "empty_battery.png")),
                 
             }
             
