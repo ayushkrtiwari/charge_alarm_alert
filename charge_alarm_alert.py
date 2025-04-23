9dd36af06409b703d21c6161adf22557e10a384e
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-23 23:49:08 +0530

    except block

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -59,5 +59,6 @@ class ChargeAlarmApp:
             self.charger_connected_image = Image.open(os.path.join(base_path, "charger_connected.jpg"))
             self.charger_disconnected_image = Image.open(os.path.join(base_path, "charger_disconnected.png"))
             print("Images loaded successfully.")
+        except 
+
         
-        # Next catch block
