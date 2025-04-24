84ec0ae456f96f504510363d82b28475658d6e6e
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-24 23:29:25 +0530

    Ctrl+Enter key event tagged

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -63,5 +63,6 @@ class ChargeAlarmApp:
             print(f"Error loading images: {e}")
 
         # Bind Ctrl + Enter to stop the alarm
-        root.bind('<Control-Return>', lambda event: self.stop_alarm_action()) # Dynamic alarm interrupt shortcut
-        
+        root.bind('<Control-Return>', lambda event: self.stop_alarm_action()) # bind key shortcut for alarm interrupt
+
+    
