7f7c7dc426ff55c0a4822d317dbfd46ceb7172f4
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-24 23:34:18 +0530

    action passed to protocol params

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -67,6 +67,6 @@ class ChargeAlarmApp:
         print("Ctrl + Enter key binding added.")
 
         # Bind window close event
-        root.protocol("WM_DELETE_WINDOW")
+        root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     
