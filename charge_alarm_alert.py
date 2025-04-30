e5c7e9312f040c9abcc3e605e48ad8fb2cecba90
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-04-30 19:35:29 +0530

    state default as tk.DISABLED

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -89,6 +89,6 @@ class ChargeAlarmApp:
     def stop_alarm_action(self):
         print("Stop Alarm button pressed.")
         self.stop_alarm()
-        self.stop_alarm_button.config(state)
+        self.stop_alarm_button.config(state=tk.DISABLED)
 
     
