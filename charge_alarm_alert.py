a7c561b875913074cf2d89d45d929ff7a8fc9f4c
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-04 23:20:14 +0530

    Battery level 80: Alarm triggered

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -110,5 +110,6 @@ class ChargeAlarmApp:
         self.is_monitoring = False
         self.start_button.config(state=tk.NORMAL)
         self.stop_button.config(state=tk.DISABLED)
-        self.stop_alarm() # stop alarm after monitoring is stopped
+        self.stop_alarm()
+        self.alarm_80_triggered
         
