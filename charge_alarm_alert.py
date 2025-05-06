28f92f3f7a9994188a1fb759420a481166738e49
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-06 23:50:24 +0530

    self instance passed

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -118,5 +118,5 @@ class ChargeAlarmApp:
         self.alarm_stopped_due_to_disconnection = False  # Reset the flag
         print("Monitoring stopped.")
 
-    def resize_image():
+    def resize_image(self):
     
