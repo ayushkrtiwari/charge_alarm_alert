94289a74c6dbc6ec50f3e6c1696c8390ef887b75
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-06 23:54:15 +0530

    width set to image size

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -118,5 +118,7 @@ class ChargeAlarmApp:
         self.alarm_stopped_due_to_disconnection = False  # Reset the flag
         print("Monitoring stopped.")
 
-    def resize_image(self, image, max_width, max_height): # method declaration for resizing image
+    def resize_image(self, image, max_width, max_height):
+        width= image.size
+
     
