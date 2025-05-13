5e73a92adc515267d970e759dabdf926d448ab79
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-13 23:58:21 +0530

    Event: charger disconnected

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -152,6 +152,7 @@ class ChargeAlarmApp:
                 charger_connected = self.is_charger_connected()
                 self.battery_label.config(text=f"Current Battery Percentage: {battery_percentage}%\nCharger Connected: {charger_connected}")
                 self.progress['value'] = battery_percentage
-                self.update_battery_image(battery_percentage, charger_connected) # image updated on specific new battery level
+                self.update_battery_image(battery_percentage, charger_connected)
 
-                
+                if not charger_connected:
+                    
