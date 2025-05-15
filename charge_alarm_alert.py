4d87bc6059de04f8d6996e228be1013636865ed9
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-15 23:20:04 +0530

    Alarm 100 turned off

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -163,4 +163,5 @@ class ChargeAlarmApp:
                         self.alarm_stopped_due_to_disconnection = True  # Flag updated to prevent Alarm Repetition
                     self.alarm_80_triggered = False
                     self.alarm_90_triggered = False
-                    
+                    self.alarm_100_triggered = False
+                
