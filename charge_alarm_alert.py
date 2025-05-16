998a4e9ccff65ab9adaf328bc2dc3f794de40332
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-16 23:24:19 +0530

    Set Alarm 100 trigerred TRUE

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -169,4 +169,5 @@ class ChargeAlarmApp:
                     self.alarm_stopped_due_to_disconnection = False 
                     if battery_percentage >= 100 and not self.alarm_100_triggered:
                         self.play_alarm('siren_100.mp3', loop=True)
+                        self.alarm_100_triggered = True
                         
