561216813ac7038c7c60fd2381e81848205d2e45
AYUSH KUMAR TIWARI <tiwariayush4373@gmail.com>
2025-05-19 23:45:52 +0530

    Event: Alarm not stopped on charger disconnection

--- charge_alarm_alert.py
+++ charge_alarm_alert.py
@@ -175,5 +175,5 @@ class ChargeAlarmApp:
                         self.play_alarm('siren_90.mp3')
                         self.alarm_90_triggered = True
                         self.root.after(500, lambda: messagebox.showwarning("Battery Alert", "Battery above 90%! Disconnect charger.\nPress Ctrl+Enter to stop alarm."))
-                    elif battery_percentage >= 80 and battery_percentage < 90 and not self.alarm_80_triggered :
+                    elif battery_percentage >= 80 and battery_percentage < 90 and not self.alarm_80_triggered and not self.alarm_stopped_due_to_disconnection:
                         
